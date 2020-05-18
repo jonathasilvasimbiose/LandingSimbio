b425658a01b0295777e88e60665811ba4362b95d
silvajohnny777 <jonatha_silva2012@hotmail.com>
2020-05-18 18:49:59 -0300

    Updates

---
+++ precache-manifest.72ec65961945b6339fe3ead4e7d41945.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a16e1a284beb6a88f4abe74d7c9b3b",
+    "url": "/LandingSimbio/index.html"
+  },
+  {
+    "revision": "fea6b7d9ce8025bb6017",
+    "url": "/LandingSimbio/static/css/main.b17720cb.chunk.css"
+  },
+  {
+    "revision": "00da743610fd7bff4015",
+    "url": "/LandingSimbio/static/js/2.85d788b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LandingSimbio/static/js/2.85d788b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea6b7d9ce8025bb6017",
+    "url": "/LandingSimbio/static/js/main.4dec6aaf.chunk.js"
+  },
+  {
+    "revision": "fa6d28108a0946d21941",
+    "url": "/LandingSimbio/static/js/runtime-main.1f2823ae.js"
+  },
+  {
+    "revision": "05e2ee773ad654f4e1cecef87648dbbf",
+    "url": "/LandingSimbio/static/media/Frame.05e2ee77.png"
+  },
+  {
+    "revision": "f4f609be32f3cc330ddd675353807c27",
+    "url": "/LandingSimbio/static/media/FrameCard2.f4f609be.png"
+  },
+  {
+    "revision": "497d3a0eaf295c2c650b61606da04403",
+    "url": "/LandingSimbio/static/media/FrameMissao.497d3a0e.png"
+  },
+  {
+    "revision": "2a49e5504d31c1928298ed049321106d",
+    "url": "/LandingSimbio/static/media/FrameMobile.2a49e550.png"
+  },
+  {
+    "revision": "221693b8e3d0167b65c705a781521e5a",
+    "url": "/LandingSimbio/static/media/buscafiltros.221693b8.png"
+  },
+  {
+    "revision": "85d51829498c66bc5be3dc7a8a26a668",
+    "url": "/LandingSimbio/static/media/dicaspersonalizadas.85d51829.png"
+  },
+  {
+    "revision": "8e7a08f4fad376cd7e5e2b60146ebd3a",
+    "url": "/LandingSimbio/static/media/leisincetivofoto.8e7a08f4.png"
+  },
+  {
+    "revision": "800415e6daf7d2a1ab41ff85a24106e5",
+    "url": "/LandingSimbio/static/media/oqueradar.800415e6.png"
+  },
+  {
+    "revision": "145bdc3a6e816aa59d972ea52f201b8f",
+    "url": "/LandingSimbio/static/media/paginaProponentes1.145bdc3a.png"
+  },
+  {
+    "revision": "fe7b52b9d999034f5521fead38d18f18",
+    "url": "/LandingSimbio/static/media/plataformaEmpresa1.fe7b52b9.png"
+  },
+  {
+    "revision": "e68b9db940e276fa65898840d1d69a7b",
+    "url": "/LandingSimbio/static/media/plataformaEmpresa2.e68b9db9.png"
+  },
+  {
+    "revision": "d96f0ab305db3edc4e9819195deef8b4",
+    "url": "/LandingSimbio/static/media/plataformaEmpresa3.d96f0ab3.png"
+  },
+  {
+    "revision": "1673aa4cc49931a09635f23c56d8e18f",
+    "url": "/LandingSimbio/static/media/plataformaProponente1.1673aa4c.png"
+  },
+  {
+    "revision": "81e43d161b0a0a92d90f6be196cc6a4f",
+    "url": "/LandingSimbio/static/media/plataformaProponente2.81e43d16.png"
+  },
+  {
+    "revision": "c81eddc761df304dd0d5d3f05c6c0e31",
+    "url": "/LandingSimbio/static/media/plataformaProponente3.c81eddc7.png"
+  },
+  {
+    "revision": "d9381de4ff14479136f7c0d3411636c1",
+    "url": "/LandingSimbio/static/media/plataformaProponente4.d9381de4.png"
+  },
+  {
+    "revision": "e9fd70481007334efb2f6e5147596237",
+    "url": "/LandingSimbio/static/media/relatoriosdetalhados.e9fd7048.png"
+  },
+  {
+    "revision": "ae31559affc1f00ed523dd821943a49a",
+    "url": "/LandingSimbio/static/media/resultadosrelevantes.ae31559a.png"
+  },
+  {
+    "revision": "43d68172c94d2edcfa769fb2460d8032",
+    "url": "/LandingSimbio/static/media/tiraDuvidas.43d68172.png"
+  }
+]);
